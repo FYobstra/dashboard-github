18a12d1beb57f1d4720cd0caece510a8ebc78997
FYobstra <facundolyobstraibizer@gmail.com>
2023-04-28 10:35:03 -0300

    bugFix

--- src/Images/index.jsx
+++ src/Images/index.jsx
@@ -1,7 +1,7 @@
 import BgLogin from "./bg-login.jpeg"
 import IconStar from "./IconStar.svg";
 import IconRepos from "./IconRepos.svg";
-import GithubLogin from "./GithubLogin.jpeg";
+import GithubLogin from "./githubLogin.jpeg";
 import IconFollowers from "./IconFollowers.svg";
 import IconFollowing from "./IconFollowing.svg";
 
